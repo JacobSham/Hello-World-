592c8f507ef183b4da12bcd16ec3f3a5ba9ad102
c6sham <c6sham@gmail.com>
2019-04-07 22:49:20 -0400

    Quick Fix to app.py

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ from hello import Hello
 app = Flask(__name__)
 api = Api(app)
 
+@app.route('/')
 def hello():
     return 'Hello World!'
 
 def test_hello():
     assert hello() =='Hello World!'
+
 
 if __name__ == '__main__':
     app.run(port=4000, debug=True)
