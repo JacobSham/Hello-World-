5ee43fab735b731d228c0c73d6ac4ecdb228f8d5
c6sham <c6sham@gmail.com>
2019-04-07 22:45:40 -0400

    Simplified Hello World to app.py and added Unit Test

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ from hello import Hello
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Hello, '/')
+def hello():
+    return 'Hello World!'
 
+def test_hello():
+    assert hello() =='Hello World!'
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+    app.run(port=4000, debug=True)
